a462d2dc1608418a2091fd65e5af06cf48321e0a
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2022-07-14 10:58:41 +0200

    Beta 0.7.8a
    Teleport from new sorpigal now gets you to the oasis.

--- Scripts/Maps/oute3.lua
+++ Scripts/Maps/oute3.lua
@@ -1,3 +1,5 @@
 -- configure shrine event
 configureShrineEvent(261, 6, "BaseLuck", 23, 24, 25, 26)
-
+evt.map[104] = function()
+	evt.MoveToMap{X = -9678, Y = -13013, Z = 0, Direction = 0, LookAngle = 0, SpeedZ = 0, HouseId = 0, Icon = 0, Name = "OutB3.Odm"}
+end
