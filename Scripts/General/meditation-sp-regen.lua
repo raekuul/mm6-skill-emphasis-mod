fcf0afc59633f2fa623d0fb668c6a56f111d2758
raekuul <raekuul@github.com>
2022-10-06 19:10:46 -0500

    Auto-Repair version 1

--- Scripts/General/meditation-sp-regen.lua
+++ Scripts/General/meditation-sp-regen.lua
@@ -1,5 +1,15 @@
 -- Add a bit of sp regeneration by meditation skill
 
+function regenAutoRepair(player, item)
+	r,m = SplitSkill(player.Skills[const.Skills.Repair])
+	cap = r * m
+	generic = item.T(item)
+	diff = generic.IdRepSt
+	if (cap >= diff) then
+		item.Broken = false
+	end
+end
+
 function calculateMeditationSPRegen(rank, mastery, fullSP)
 	fixed = mastery
 	scaled = math.ceil(fullSP^0.5 * mastery^2/400)
@@ -9,7 +19,15 @@ end
 
 function events.Regeneration(t)
 	v = Party[t.PlayerIndex]
-	
+	ko = v.Eradicated or v.Dead or v.Stoned or v.Paralyzed or v.Unconscious
+	if (ko == 0) then
+		for k=1, v.Items.High do
+			if (v.Items[k].Broken == true) then
+				regenAutoRepair(v,v.Items[k])
+			end
+		end
+	end
+
 	r,m = SplitSkill(v.Skills[const.Skills.Meditation])
 	cap = v:GetFullSP()
 	cur = v.SpellPoints
