b036582f4857d6e7d0329d3043f00d7a5e21b685
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2022-11-16 11:36:07 +0100

    Delete bowfix.lua
    Internalized in skill-mod.lua

--- Scripts/General/bowfix.lua
+++
@@ -1,6 +0,0 @@
-function events.ModifyItemDamage(t)
-local s, m = SplitSkill(t.Player.Skills[const.Skills.Bow])
-	if t.Item:T().EquipStat == const.ItemType.Missile - 1 then
-		t.Result = t.Result + s * (m <= const.Expert and m or 2)
-	end
-end
